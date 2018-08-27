2d4086ff5daefe5dbbbca97fc41f10e7b04613ba
mahadikPrathamesh <36527725+mahadikPrathamesh@users.noreply.github.com>
2018-08-27 10:30:46 +0530

    home page

--- spalsh_Activity.java
+++ spalsh_Activity.java
@@ -1,5 +1,7 @@
 package com.projects.prathamesh.imagecompressor;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,26 @@ public class spalsh_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spalsh_);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        new CountDownTimer(4000,100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                Intent homePage = new Intent(getApplicationContext(),Home_Activity.class);
+                startActivity(homePage);
+            }
+        }.start();
     }
 }
